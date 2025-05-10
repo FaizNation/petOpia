40a1eef6a59c6a4d791aadc4dac84aff2392a797
Faiznation <novaatalagrab@gmail.com>
2025-05-10 14:14:46 +0700

    initial commit

---
+++ src/main/java/FaizNation/petOpia_dev/PetOpiaDevApplication.java
@@ -0,0 +1,13 @@
+package FaizNation.petOpia_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetOpiaDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetOpiaDevApplication.class, args);
+	}
+
+}
