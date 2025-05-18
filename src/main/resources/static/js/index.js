83370507758ee464d8c6b227eda598bc061471b6
Faiznation <novaatalagrab@gmail.com>
2025-05-18 11:36:19 +0700

    Modified: index.html

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,43 +1,35 @@
-gsap.registerPlugin(ScrollTrigger)
+jQuery("#carousel").owlCarousel({
+  autoplay: true,
+  rewind: false, /* use rewind if you don't want loop */
+  margin: 20,
+  loop: true,
+   /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  nav: true,
+  navText: [
+    '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>',
+    '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
+  responsive: {
+    0: {
+      items: 1
+    },
 
-        const splitTypes = document.querySelectorAll('.reveal-type')
+    600: {
+      items: 3
+    },
 
-        splitTypes.forEach((char,i) => {
+    1024: {
+      items: 4
+    },
 
-            const bg = char.dataset.bgColor
-            const fg = char.dataset.fgColor
-
-            const text = new SplitType(char, { types: 'chars'})
-
-            gsap.fromTo(text.chars, 
-                {
-                    color: bg,
-                },
-                {
-                    color: fg,
-                    duration: 0.3,
-                    stagger: 0.02,
-                    scrollTrigger: {
-                        trigger: char,
-                        start: 'top 80%',
-                        end: 'top 20%',
-                        scrub: true,
-                        markers: false,
-                        toggleActions: 'play play reverse reverse'
-                    }
-            })
-        })
-
-
-        const lenis = new Lenis()
-
-        lenis.on('scroll', (e) => {
-        console.log(e)
-        })
-
-        function raf(time) {
-        lenis.raf(time)
-        requestAnimationFrame(raf)
-        }
-
-        requestAnimationFrame(raf)
+    1366: {
+      items: 4
+    }
+  }
+});
